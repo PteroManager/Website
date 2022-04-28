e53f6ff355208881f0c61365b98f30c4ac2bb8f7
DottoXD <80960175+DottoXD@users.noreply.github.com>
2022-04-28 20:56:48 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,1 +1,12 @@
-<template></template>
+<template>
+  <div id="navbar">
+    <nav>
+      <ul>
+        <li><a href="https://github.com/PteroManager">GitHub</a></li>
+        <li><a href="https://npmjs.com/package/PteroManager">NPM</a></li>
+        <li><a href="https://npmjs.com/package/@PteroManager/Builders">@PteroManager/Builders</a></li>
+        <li><a href="https://npmjs.com/package/@PteroManager/Collection">@PteroManager/Collection</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
