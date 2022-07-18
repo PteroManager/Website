b78e45c29fc798504b88e013861b823667bbc728
Jeweetzelf <67839219+jeweetzelfWasTaken@users.noreply.github.com>
2022-07-18 13:37:29 +0200

    f djoh

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,7 @@
   <h3>
     Copyright @ 2022 PteroManager, brought to you by:
     <a href="https://github.com/DottoXD">Dotto</a>,
-    <a href="https://github.com/jeweetzelfWasTaken">Jeweetzelf</a>,
-    <a href="https://github.com/JustArtiom">Artiom</a> and
-    <a href="https://github.com/itzJOHv">Djoh</a>
+    <a href="https://github.com/jeweetzelfWasTaken">Jeweetzelf</a> and 
+    <a href="https://github.com/JustArtiom">Artiom</a>
   </h3>
 </template>
